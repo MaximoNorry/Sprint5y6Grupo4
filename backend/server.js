6330d1c3e9ba577d467616a0cc8c7fa831d456bb
MaximoNorry <maxiurcmonaco@gmail.com>
2025-11-20 05:28:10 -0300

    instalo cors para el vercel

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,22 @@ const productRoutes = require('./routes/productRoutes');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 4000;
-app.use(cors());
+
+app.use(cors({
+  origin: "https://sprint5y6.vercel.app"
+}));
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
 app.use('/api/productos', productRoutes);
+
 app.get('/', (req, res) => res.send('API Sprint5y6 - Backend funcionando'));
+
 const mongoUri = process.env.MONGO_URI;
 if (!mongoUri) {
   console.error('Error: MONGO_URI no definido en .env');
   process.exit(1);
 }
+
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Conectado a MongoDB Atlas');
