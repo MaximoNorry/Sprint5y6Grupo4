1d245722bb1107bf225d1b894a33afa4bca02522
MaximoNorry <maxiurcmonaco@gmail.com>
2025-11-20 05:41:52 -0300

    actualizo api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "https://sprint5y6grupo4.onrender.com";
 
 export async function getProductos() {
   const res = await fetch(`${BASE_URL}/api/productos`);
